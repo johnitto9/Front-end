e4d355eb7d905eb80fc3e7528197270b1aa94b5c
johnitto9 <109311731+johnitto9@users.noreply.github.com>
2022-09-13 03:03:02 -0300

    Se agrega proyectos y footer

--- miPortfolio/src/app/app.module.ts
+++ miPortfolio/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkilsComponent } from './components/skils/skils.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { SkilsComponent } from './components/skils/skils.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkilsComponent
+    SkilsComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
